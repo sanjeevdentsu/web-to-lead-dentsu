02013c4a107ff2821cf4298cd9a56b39f8cf92ce
sanjeevdentsu <110912496+sanjeevdentsu@users.noreply.github.com>
2022-08-09 18:58:02 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ function Login() {
   return (
     <>
       <div>
-        <a href="https://dentsuworldservices-8b-dev-ed.my.salesforce.com/services/oauth2/authorize?client_id=3MVG9wt4IL4O5wvJidVoZyfx_jUvv0N3NT.Ln0NsG_S8TgSMmTtSsJ1nDQthfOIJLxIgEfE28B_N42dlU88cH&redirect_uri=https://www.google.com&response_type=code">Login</a>
+        <a href="https://dentsuworldservices-8b-dev-ed.my.salesforce.com/services/oauth2/authorize?client_id=3MVG9wt4IL4O5wvJidVoZyfx_jUvv0N3NT.Ln0NsG_S8TgSMmTtSsJ1nDQthfOIJLxIgEfE28B_N42dlU88cH&redirect_uri=">Login</a>
       </div>
     </>
   )
